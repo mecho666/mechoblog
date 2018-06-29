c9581c386699959ea9cf2179c334161dfcaa6b8a
mecho666 <wy553171@qq.com>
2018-06-29 18:29:42 +0800

    添加了数据库配置信息

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
+    #配置数据库信息
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    #配置项用于设置数据发生变更之后是否发送信号给应用
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
